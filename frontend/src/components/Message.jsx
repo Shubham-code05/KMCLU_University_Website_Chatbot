7aeaf6b8b74f2e7576c9cf3a26d28bdedaecbf5a
Shubham-code05 <shubham.cse2022@gmail.com>
2026-04-16 21:24:04 +0530

    make KMCLU links clickable

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,7 +1,12 @@
 export default function Message({ text, sender }) {
+  const formattedText = text.replace(
+    /(https?:\/\/[^\s]+)/g,
+    '<a href="$1" target="_blank" style="color:#0b5ed7; text-decoration: underline;">$1</a>'
+  );
+
   return (
     <div className={`message ${sender}`}>
-      {text}
+      <span dangerouslySetInnerHTML={{ __html: formattedText }} />
     </div>
   );
 }
