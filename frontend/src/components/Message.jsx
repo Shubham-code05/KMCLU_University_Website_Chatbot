99ba5eb22fba2ef8b4ee1a9c0db4bc518cfa7434
Shubham-code05 <shubham.cse2022@gmail.com>
2026-04-17 00:23:17 +0530

    fix clickable links and line breaks

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,11 +1,16 @@
 export default function Message({ text, sender }) {
-  const formattedText = text.replace(
-    /(https?:\/\/[^\s]+)/g,
-    '<a href="$1" target="_blank" style="color:#0b5ed7; text-decoration: underline;">$1</a>'
-  );
+  const formattedText = text
+    .replace(
+      /(https?:\/\/[^\s]+)/g,
+      '<a href="$1" target="_blank" rel="noopener noreferrer" style="color:#0b5ed7; text-decoration: underline;">$1</a>'
+    )
+    .replace(/\n/g, "<br />");
 
   return (
-    <div className={`message ${sender}`}>
+    <div
+      className={`message ${sender}`}
+      style={{ whiteSpace: "pre-wrap" }}
+    >
       <span dangerouslySetInnerHTML={{ __html: formattedText }} />
     </div>
   );
