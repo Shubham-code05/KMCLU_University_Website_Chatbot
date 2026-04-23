a09e4682c73f352e883b0f9513f02fd2c9c5d275
Shubham-code05 <shubham.cse2022@gmail.com>
2026-04-23 22:35:47 +0530

    fix: vite base path for vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/KMCLU_University_Website_Chatbot/",
+  base: "/",   // 🔥 ye change important hai
   plugins: [
     react(),
     tailwindcss(),
